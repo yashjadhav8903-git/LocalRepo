6fb1672c177979d3fea7ef2d38b5caae26d0be25
yashjadhav8903-git <yashjadhav8903@gmail.com>
2025-12-03 23:28:07 +0530

    code

---
+++ Code.java
@@ -0,0 +1,14 @@
+package LocalRepo;
+
+public class Code {
+    public static void main(String[] args) {
+        int a = 11;
+        //int b = 12;
+
+        if(a % 2 == 0){
+            System.out.println("Number is Even");
+        } else{
+            System.out.println("Number is Old");
+        }
+    }
+}
